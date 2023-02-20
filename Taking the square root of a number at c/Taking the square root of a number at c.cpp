c93cefc609f6ca6e1cbee2823284e77cb9e11e88
MuhammetSanli <114609160+MuhammettSanli@users.noreply.github.com>
2023-02-20 13:24:12 +0300

    Add files via upload

---
+++ Taking the square root of a number at c/Taking the square root of a number at c.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main() {
+	
+	int number;
+	double result;
+	
+	printf("Write a number");
+	scanf("%d",&number);
+	result=sqrt(number);
+	printf("Result=%f",result);
+	
+	
+	
+	
+	
+	
+	return 0;
+}
